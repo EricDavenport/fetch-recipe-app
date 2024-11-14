687cfc18b0c7935912aa052a6a9794418c74361c
Eric Davenport <Ericdavenport@pursuit.org>
2024-11-14 18:29:55 -0500

    constants file

--- FetchRecipeApp/Utilities/Constants.swift
+++ FetchRecipeApp/Utilities/Constants.swift
@@ -1,1 +1,6 @@
+struct Constants {
+    static let allRecipeURL = "https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json"
+    static let malformedURL = "https://d3jbb8n5wk0qxi.cloudfront.net/recipes-malformed.json"
+    static let emptyDataURL = "https://d3jbb8n5wk0qxi.cloudfront.net/recipes-empty.json"
+}
 
