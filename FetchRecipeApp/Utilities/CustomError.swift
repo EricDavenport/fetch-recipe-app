8caee6b9a1d34fd3e88a9721e0237cadb7d8fc52
Eric Davenport <Ericdavenport@pursuit.org>
2024-11-14 18:29:04 -0500

    custom error file added

--- FetchRecipeApp/Utilities/CustomError.swift
+++ FetchRecipeApp/Utilities/CustomError.swift
@@ -1,1 +1,25 @@
+import Foundation
+
+enum CustomError: LocalizedError {
+    case networkError
+    case badData
+    case emptyData
+    case malformedData
+    case unknownData
+
+    var errorDescription: String? {
+        switch self {
+            case .networkError:
+                return "Network connection issue. Please check your internet connection."
+            case .badData:
+                return "Failed to load recipes due to data issues"
+            case .emptyData:
+                return "No recipes available at the moment."
+            case .malformedData:
+                return "Data received from the server was malformed."
+            case .unknownData:
+                return "An unknown error occurred. Please try again later."
+        }
+    }
+}
 
